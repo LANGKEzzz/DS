7217c732477361f96d7c94fb16e1ca3bd1fbc22a
LANGKEzzz <LANGKEzzz>
2019-01-06 16:25:47 +0800

    common配置

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from "./store";
+import "./common/css/reset.css";
+import "../node_modules/swiper/dist/css/swiper.min.css";
+import "./common/js/flexble";
 
 Vue.config.productionTip = false
 
@@ -10,6 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
