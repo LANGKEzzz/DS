9b79c4b2a3884d5f8827ca742d4632b80aecc424
LANGKEzzz <LANGKEzzz>
2019-01-06 18:16:42 +0800

    111

---
+++ app/src/components/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    
+    <Register></Register>
+</template>
+
+<script>
+import Register from "./components/reg.vue";
+export default {
+    components:{
+        "Register":Register
+    }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
+
