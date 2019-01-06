4f43c2d57bbb051010ad1ecc1aaf8dc7427bf5c1
15822717214 <1412817622@qq.com>
2019-01-06 21:14:48 +0800

    第一次提交

---
+++ app/src/components/my/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="header">
+	   <h2>我</h2>
+	   <img class="img1" src="../../../../static/head.png">
+	    <img class="img2" src="../../../../static/shizhi.png">
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	#header{
+		width:7.5rem;
+		height:4.22rem;
+		background:skyblue;
+		/*background:url("../../../../static/lf.jpg") no-repeat center;
+		background-size:cover;*/
+		.img1{
+			width:1.54rem;
+			height:1.54rem;
+		}
+		.img2{
+			width:0.42rem;
+			height:0.42rem;
+		}
+	}
+</style>
---
+++ app/src/components/my/components/main.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="main">
+		<div class="ex"></div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+	#main{
+		width:7.5rem;
+	    height:10.1rem;
+	    background:yellow;
+	    .ex{
+	    	width:7.5rem;
+	    	height:0.2rem;
+	    	background:gray;
+	    }
+	}
+	
+</style>
