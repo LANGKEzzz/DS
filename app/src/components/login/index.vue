c89e08e130a8c632c314f22ca8dd390f42df3829
LANGKEzzz <LANGKEzzz>
2019-01-07 20:53:50 +0800

    登录注册静态页面

--- app/src/components/login/index.vue
+++ app/src/components/login/index.vue
@@ -1,19 +1,105 @@
 <template>
-    
-    <!-- <Register></Register> -->
+  <div class="app">
+    <div class="logo">
+      <img :src="logourl">
+    </div>
+    <div class="dl_zh">
+      <input type="text" placeholder="账号/手机号" class="num">
+    </div>
+    <div class="dlpwd">
+      <input type="text" class="pwd" placeholder="密码">
+    </div>
+    <div class="dl">
+        <button class="dlbtn">登录</button>
+    </div>
+    <p class="forget">忘记密码</p>
+    <router-link :to="{name:'register'}">
+        <p class="go_reg">没有账号？去注册></p>
+    </router-link>
+  </div>
 </template>
 
 <script>
-// import Register from "./components/reg.vue";
 export default {
-    components:{
-        // "Register":Register
-    }
-}
+  data() {
+    return {
+      logourl: "../../../static/logo/logo@2x.png"
+    };
+  }
+};
 </script>
 
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.app {
+  width: 100%;
+  height: 100%;
+  .logo {
+    width: 4.14rem;
+    height: 2.6rem;
+    margin: 0 auto;
+    margin-top: 1.83rem;
+    img {
+      width: 4.14rem;
+      height: 2.6rem;
+    }
+  }
+  .dl_zh,
+  .dlpwd,.dl {
+    width: 100%;
+    height: 1rem;
+  }
+  .dl_zh{
+      margin-top: 1.09rem;
+  }
+  .dlpwd{
+      margin-top: 0.61rem;
+  }
+  .dl{
+      margin-top:.93rem;
+      .dlbtn{
+          display:block;
+          margin: 0 auto;
+          background: #63D7D4;
+          color: white;
+          width: 6.04rem;
+            height: 1rem;
+            border-radius: 0.5rem;
+            border: none;
+            outline: none;
+             box-shadow: 3px 8px 25px 0px rgba(44, 96, 95, 0.25);
+             
+      }
+  }
+  .num,
+  .pwd {
+    width: 6.04rem;
+    height: 1rem;
+    border-radius: 0.5rem;
+    border: none;
+    text-indent: 1.04rem;
+    outline: none;
+    display: block;
+    margin: 0 auto;
+    box-shadow: 3px 8px 25px 0px rgba(44, 96, 95, 0.25);
+    font-size: 0.26rem;
+  }
+  .forget{
+      width: 1rem;
+      font-size: 0.22rem;
+      text-align: center;
+      margin: 0 auto;
+      margin-top: 0.52rem;  
+      color: #63D7D4;    
+  }
+  .go_reg{
+      width: 2.2rem;
+      height: .25rem;
+      margin: 0 auto;
+      margin-top: 2rem;
+      text-align: center;
+      color: #888;
+  }
+}
 </style>
 
