2d843bd3c2e91c3348d3618329dd6c6adf174c18
zln1837043024 <45353797+zln1837043024@users.noreply.github.com>
2019-01-25 10:46:45 +0800

    Update mutations.js

--- app/src/store/my/mutations.js
+++ app/src/store/my/mutations.js
@@ -6,10 +6,7 @@ export default{
     handleCollectData(state,params){
         console.log(params)
         state.arrCollect = params
-        console.log(state.arrCollect)
-<<<<<<< HEAD
-    }
-=======
+ 
     },
     handleMyData(state,params){
         console.log(params)
@@ -17,5 +14,5 @@ export default{
         console.log(state.arrMy)
     }
 
->>>>>>> 716a0de049a9848d8cd78d9e02c43585fa045a97
+
 }
